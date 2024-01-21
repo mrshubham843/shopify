4deab891fcd666ee0da7ff73c03ea940fcbc8fd4
mrshubham843 <85013820+mrshubham843@users.noreply.github.com>
2024-01-21 18:27:41 +0530

    Update constant.php

--- web/includes/constant.php
+++ web/includes/constant.php
@@ -1,15 +1,15 @@
 <?php
 $GLOBALS['ngRokUrl'] = 'https://f595-49-34-213-55.ngrok-free.app';
-$GLOBALS['projectDir'] = '/SocialX';
+$GLOBALS['projectDir'] = '/';
 $GLOBALS['dbServer'] = 'localhost';
 $GLOBALS['dbUsername'] = 'root';
 $GLOBALS['dbPassword'] = '';
-$GLOBALS['dbDatabase'] = 'simpleapp';
-$GLOBALS['appApiKey'] = '3c66fdcb5846b9087ce9b6293b78de41';
-$GLOBALS['appApiSecretKey'] = '33915d872ec1d412cc2dd5c52798d3e8';
+$GLOBALS['dbDatabase'] = '';
+$GLOBALS['appApiKey'] = '';
+$GLOBALS['appApiSecretKey'] = '';
 $GLOBALS['appScopes'] = 'read_products,write_products,read_script_tags,write_script_tags';
-$GLOBALS['appUrlId'] = 'simple-app-82';
-$GLOBALS['scriptUrl'] = $GLOBALS['ngRokUrl'] . $GLOBALS['projectDir'] . '/scripts/elana.js';
-$GLOBALS['SSM_API_URL'] = 'https://cheapestsmmpanels.com/api/v2';
-$GLOBALS['SSM_API_TOKEN'] = 'a8aaa7f90ae18e832feaf137c5a3c4b3';
-$GLOBALS['YOUTUBE_API_KEY'] = 'AIzaSyBeii1qQ_eZ_7-8gA9Jy5Hr0X9HaZAdSp8';
+$GLOBALS['appUrlId'] = 'simple-app';
+$GLOBALS['scriptUrl'] = $GLOBALS['ngRokUrl'] . $GLOBALS['projectDir'] . '/scripts/adsasdsd.js';
+$GLOBALS['SSM_API_URL'] = 'om/api/v2';
+$GLOBALS['SSM_API_TOKEN'] = '';
+$GLOBALS['YOUTUBE_API_KEY'] = '-8gA9Jy5Hr0X9HaZAdSp8';
